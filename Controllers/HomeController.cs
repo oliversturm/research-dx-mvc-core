203203d0a614f3b79deab2a2f2337bb3b6a17284
Oliver Sturm <oliver@oliversturm.com>
2017-05-25 11:56:09 +0100

    showing basic data grid

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,27 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
+using research_dx_mvc_core.Models;
+
 namespace research_dx_mvc_core.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
+            var model = new List<Person> {
+                new Person { Name = "Bob", Age = 24 },
+                new Person { Name = "Anna", Age = 42 },
+                new Person { Name = "Oliver", Age = 36 }
+            };
+            return View(model);
         }
 
         public IActionResult Error()
---
+++ Models/Person.cs
@@ -0,0 +1,6 @@
+namespace research_dx_mvc_core.Models {
+    public class Person {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
